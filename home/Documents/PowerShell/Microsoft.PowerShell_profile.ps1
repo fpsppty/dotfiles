2637a15f31f6b58ca6a3e00a1b680ab9b3860109
fpsppty <128275033+fpsppty@users.noreply.github.com>
2026-03-13 21:58:37 +0900

    chore(config): 🗑️ 不要な設定ファイルとスタイルシートを削除
    * `theme.omp.json`, `config.yaml`, `styles.css` を削除しました。
    * 不要なファイルを整理し、プロジェクトのクリーンアップを実施しました。

--- home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,18 @@ if (Get-Command starship -ErrorAction SilentlyContinue) {
 if (Get-Command mise -ErrorAction SilentlyContinue) {
     mise activate pwsh | Out-String | Invoke-Expression
 }
+
+# PSReadLine settings
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# PSFzf settings
+Import-Module PSFzf
+Set-PSFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+function q {
+    ghq list -p | fzf | ForEach-Object {
+        if ($_ -ne '') { Set-Location $_ }
+    }
+}
